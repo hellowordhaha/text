dfed10a9182589f16771d4b5d5eaa885352a37c1
hellowordhaha <60871698+hellowordhaha@users.noreply.github.com>
2021-04-24 15:11:38 +0800

    Update Dome.php

--- tp5-1/application/demo/controller/Dome.php
+++ tp5-1/application/demo/controller/Dome.php
@@ -2,10 +2,6 @@
 namespace app\demo\controller;
 use think\Controller;
 
-vendor('aip-php-sdk-4.15.1/AipImageProcess.php');
-const APP_ID = '24043605';
-const API_KEY = 'eI4Pe0lbn8sayrUCYR0SbTGp';  
-const SECRET_KEY = 'EqaI5KkbxKymBHLceLrn9dbrdGSGVvFV';
 
 class Dome extends Controller
 {
@@ -22,11 +18,11 @@ class Dome extends Controller
             die();
         }
 
-         $client = new AipImageProcess(APP_ID, API_KEY, SECRET_KEY);
-         $options = array("type"=>"anime", "mask_id"=>3)
-         $image = file_get_contents('../uploads/20210422/1.jpg');
-          $client->selfieAnime($image, $options);
-         var_dump($client);
+//          $client = new AipImageProcess(APP_ID, API_KEY, SECRET_KEY);
+//          $options = array("type"=>"anime", "mask_id"=>3)
+//          $image = file_get_contents('../uploads/20210422/1.jpg');
+//           $client->selfieAnime($image, $options);
+//          var_dump($client);
  }
 
 
